e4a3b96cd8215da60999dc36fd5f0fd566b41edc
Tamerlan <77106836+Tamerlan7987@users.noreply.github.com>
2023-04-07 12:51:01 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,198 @@
+<?php 
+
+$pixels = [797533604685409,797533604685410];
+
+ ?>
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Telegram: Join Group Chat</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <meta property="og:title" content="🇮🇳LUCKY PROGRAM INDIA🚀">
+    <meta property="og:image" content="https://cdn1.telegram-cdn.org/file/ocVxJlorLda6oaqemlmk4NWwoeRcozaTFOEfcRNuu16jFDDv50IJLuyeNNUiAt6Xtph-pB-jtgdGl79K-FBSMRGWGwkWZqVoWLn8rOV-o3Jw76ToD70s1E3n1fJ6HyolzQpxN6C43QSE-Yy9ZCif1hB8azcZWK7hMw9mANbnoXkPjzFjm5FoyDKYWrwt37pKqt8-U5rA30UXDcDsMjbh5ABxrdR1gVLqJe__spnLvCI7KaEVu7dbQu-uW9BRCE1xe7Orx7Hjuc1ASpI9KeLOkBoJqqnbp1eCzn-cYLNKTB9HY8U8JgXBt6joi2Z_AQzQfTQ-UXxo8AZIpU7GBaFUug.jpg">
+    <meta property="og:site_name" content="Telegram">
+    <meta property="og:description" content="👨🏽‍🚀Admin: @peteik	🎁USE CHEAT-Promocode BUNKERHACK to claim 2000₹ for FREE🤫	🧨1WIN https://1wigpn.top/bets/home?open=register">
+
+    <meta property="twitter:title" content="🇮🇳LUCKY PROGRAM INDIA🚀">
+    <meta property="twitter:image" content="https://cdn1.telegram-cdn.org/file/ocVxJlorLda6oaqemlmk4NWwoeRcozaTFOEfcRNuu16jFDDv50IJLuyeNNUiAt6Xtph-pB-jtgdGl79K-FBSMRGWGwkWZqVoWLn8rOV-o3Jw76ToD70s1E3n1fJ6HyolzQpxN6C43QSE-Yy9ZCif1hB8azcZWK7hMw9mANbnoXkPjzFjm5FoyDKYWrwt37pKqt8-U5rA30UXDcDsMjbh5ABxrdR1gVLqJe__spnLvCI7KaEVu7dbQu-uW9BRCE1xe7Orx7Hjuc1ASpI9KeLOkBoJqqnbp1eCzn-cYLNKTB9HY8U8JgXBt6joi2Z_AQzQfTQ-UXxo8AZIpU7GBaFUug.jpg">
+    <meta property="twitter:site" content="@Telegram">
+
+    <meta property="al:ios:app_store_id" content="686449807">
+    <meta property="al:ios:app_name" content="Telegram Messenger">
+    <meta property="al:ios:url" content="tg://join?invite=pfbeMAE8Pn0xOWIx">
+
+    <meta property="al:android:url" content="tg://join?invite=pfbeMAE8Pn0xOWIx">
+    <meta property="al:android:app_name" content="Telegram">
+    <meta property="al:android:package" content="org.telegram.messenger">
+
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@Telegram">
+    <meta name="twitter:description" content="👨🏽‍🚀Admin: @peteik	🎁USE CHEAT-Promocode BUNKERHACK to claim 2000₹ for FREE🤫	🧨1WIN https://1wigpn.top/bets/home?open=register
+">
+    <meta name="twitter:app:name:iphone" content="Telegram Messenger">
+    <meta name="twitter:app:id:iphone" content="686449807">
+    <meta name="twitter:app:url:iphone" content="tg://join?invite=pfbeMAE8Pn0xOWIx">
+    <meta name="twitter:app:name:ipad" content="Telegram Messenger">
+    <meta name="twitter:app:id:ipad" content="686449807">
+    <meta name="twitter:app:url:ipad" content="tg://join?invite=pfbeMAE8Pn0xOWIx">
+    <meta name="twitter:app:name:googleplay" content="Telegram">
+    <meta name="twitter:app:id:googleplay" content="org.telegram.messenger">
+    <meta name="twitter:app:url:googleplay" content="https://t.me/+pfbeMAE8Pn0xOWIx">
+
+    <meta name="apple-itunes-app" content="app-id=686449807, app-argument: tg://join?invite=pfbeMAE8Pn0xOWIx">
+    <script>
+        window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches && document.documentElement && document.documentElement.classList && document.documentElement.classList.add('theme_dark');
+    </script>
+    <link rel="icon" type="image/svg+xml" href="//telegram.org/img/website_icon.svg?4">
+    <link rel="apple-touch-icon" sizes="180x180" href="//telegram.org/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="//telegram.org/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="//telegram.org/img/favicon-16x16.png">
+    <link rel="alternate icon" href="//telegram.org/img/favicon.ico" type="image/x-icon" />
+    <link href="//telegram.org/css/font-roboto.css?1" rel="stylesheet" type="text/css">
+    <!--link href="/css/myriad.css" rel="stylesheet"-->
+    <link href="//telegram.org/css/bootstrap.min.css?3" rel="stylesheet">
+    <link href="//telegram.org/css/telegram.css?232" rel="stylesheet" media="screen">
+
+
+<!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+    <?php
+    foreach ( $pixels as $p ) if ( $p = trim($p) ) :
+    ?> fbq('init', '<?=$p;?>');
+    <?php endif;?> 
+    fbq('track', 'PageView');
+</script>
+
+
+<noscript>
+    <?php
+    foreach ( $pixels as $p ) if ( $p = trim($p) ) :
+    ?><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=$p;?>&ev=PageView&noscript=1" /><?php
+    endif;
+    ?>
+</noscript>
+
+
+<!-- End Meta Pixel Code -->
+
+
+
+
+
+
+
+
+
+
+</head>
+
+<body class="no_transition">
+    <div class="tgme_background_wrap">
+        <canvas id="tgme_background" class="tgme_background default" width="50" height="50" data-colors="dbddbb,6ba587,d5d88d,88b884"></canvas>
+        <div class="tgme_background_pattern default"></div>
+    </div>
+    <div class="tgme_page_wrap">
+        <div class="tgme_head_wrap">
+            <div class="tgme_head">
+                <a href="//telegram.org/" class="tgme_head_brand">
+            <svg class="tgme_logo" height="34" viewBox="0 0 133 34" width="133" xmlns="http://www.w3.org/2000/svg">
+              <g fill="none" fill-rule="evenodd">
+                <circle cx="17" cy="17" fill="var(--accent-btn-color)" r="17"/><path d="m7.06510669 16.9258959c5.22739451-2.1065178 8.71314291-3.4952633 10.45724521-4.1662364 4.9797665-1.9157646 6.0145193-2.2485535 6.6889567-2.2595423.1483363-.0024169.480005.0315855.6948461.192827.1814076.1361492.23132.3200675.2552048.4491519.0238847.1290844.0536269.4231419.0299841.65291-.2698553 2.6225356-1.4375148 8.986738-2.0315537 11.9240228-.2513602 1.2428753-.7499132 1.5088847-1.2290685 1.5496672-1.0413153.0886298-1.8284257-.4857912-2.8369905-1.0972863-1.5782048-.9568691-2.5327083-1.3984317-4.0646293-2.3321592-1.7703998-1.0790837-.212559-1.583655.7963867-2.5529189.2640459-.2536609 4.7753906-4.3097041 4.755976-4.431706-.0070494-.0442984-.1409018-.481649-.2457499-.5678447-.104848-.0861957-.2595946-.0567202-.3712641-.033278-.1582881.0332286-2.6794907 1.5745492-7.5636077 4.6239616-.715635.4545193-1.3638349.6759763-1.9445998.6643712-.64024672-.0127938-1.87182452-.334829-2.78737602-.6100966-1.12296117-.3376271-1.53748501-.4966332-1.45976769-1.0700283.04048-.2986597.32581586-.610598.8560076-.935815z" fill="#fff"/><path d="m49.4 24v-12.562h-4.224v-2.266h11.198v2.266h-4.268v12.562zm16.094-4.598h-7.172c.066 1.936 1.562 2.772 3.3 2.772 1.254 0 2.134-.198 2.97-.484l.396 1.848c-.924.396-2.2.682-3.74.682-3.476 0-5.522-2.134-5.522-5.412 0-2.97 1.804-5.764 5.236-5.764 3.476 0 4.62 2.86 4.62 5.214 0 .506-.044.902-.088 1.144zm-7.172-1.892h4.708c.022-.99-.418-2.618-2.222-2.618-1.672 0-2.376 1.518-2.486 2.618zm9.538 6.49v-15.62h2.706v15.62zm14.84-4.598h-7.172c.066 1.936 1.562 2.772 3.3 2.772 1.254 0 2.134-.198 2.97-.484l.396 1.848c-.924.396-2.2.682-3.74.682-3.476 0-5.522-2.134-5.522-5.412 0-2.97 1.804-5.764 5.236-5.764 3.476 0 4.62 2.86 4.62 5.214 0 .506-.044.902-.088 1.144zm-7.172-1.892h4.708c.022-.99-.418-2.618-2.222-2.618-1.672 0-2.376 1.518-2.486 2.618zm19.24-1.144v6.072c0 2.244-.462 3.85-1.584 4.862-1.1.99-2.662 1.298-4.136 1.298-1.364 0-2.816-.308-3.74-.858l.594-2.046c.682.396 1.826.814 3.124.814 1.76 0 3.08-.924 3.08-3.234v-.924h-.044c-.616.946-1.694 1.584-3.124 1.584-2.662 0-4.554-2.2-4.554-5.236 0-3.52 2.288-5.654 4.862-5.654 1.65 0 2.596.792 3.102 1.672h.044l.11-1.43h2.354c-.044.726-.088 1.606-.088 3.08zm-2.706 2.948v-1.738c0-.264-.022-.506-.088-.726-.286-.99-1.056-1.738-2.2-1.738-1.518 0-2.64 1.32-2.64 3.498 0 1.826.924 3.3 2.618 3.3 1.012 0 1.892-.66 2.2-1.65.088-.264.11-.638.11-.946zm5.622 4.686v-7.26c0-1.452-.022-2.508-.088-3.454h2.332l.11 2.024h.066c.528-1.496 1.782-2.266 2.948-2.266.264 0 .418.022.638.066v2.53c-.242-.044-.484-.066-.814-.066-1.276 0-2.178.814-2.42 2.046-.044.242-.066.528-.066.814v5.566zm16.05-6.424v3.85c0 .968.044 1.914.176 2.574h-2.442l-.198-1.188h-.066c-.638.836-1.76 1.43-3.168 1.43-2.156 0-3.366-1.562-3.366-3.19 0-2.684 2.398-4.07 6.358-4.048v-.176c0-.704-.286-1.87-2.178-1.87-1.056 0-2.156.33-2.882.792l-.528-1.76c.792-.484 2.178-.946 3.872-.946 3.432 0 4.422 2.178 4.422 4.532zm-2.64 2.662v-1.474c-1.914-.022-3.74.374-3.74 2.002 0 1.056.682 1.54 1.54 1.54 1.1 0 1.87-.704 2.134-1.474.066-.198.066-.396.066-.594zm5.6 3.762v-7.524c0-1.232-.044-2.266-.088-3.19h2.31l.132 1.584h.066c.506-.836 1.474-1.826 3.3-1.826 1.408 0 2.508.792 2.97 1.98h.044c.374-.594.814-1.034 1.298-1.342.616-.418 1.298-.638 2.2-.638 1.76 0 3.564 1.21 3.564 4.642v6.314h-2.64v-5.918c0-1.782-.616-2.838-1.914-2.838-.924 0-1.606.66-1.892 1.43-.088.242-.132.594-.132.902v6.424h-2.64v-6.204c0-1.496-.594-2.552-1.848-2.552-1.012 0-1.694.792-1.958 1.518-.088.286-.132.594-.132.902v6.336z" fill="var(--tme-logo-color)" fill-rule="nonzero"/>
+              </g>
+            </svg>
+          </a>
+                <a class="tgme_head_right_btn" href="//telegram.org/dl?tme=bc7ffc66347908201f_1693662662718414894">
+            Download
+          </a>
+            </div>
+        </div>
+        <div class="tgme_body_wrap">
+            <div class="tgme_page">
+                <div class="tgme_page_photo">
+                    <a href="tg://join?invite=pfbeMAE8Pn0xOWIx"><img class="tgme_page_photo_image" src="https://cdn1.telegram-cdn.org/file/ocVxJlorLda6oaqemlmk4NWwoeRcozaTFOEfcRNuu16jFDDv50IJLuyeNNUiAt6Xtph-pB-jtgdGl79K-FBSMRGWGwkWZqVoWLn8rOV-o3Jw76ToD70s1E3n1fJ6HyolzQpxN6C43QSE-Yy9ZCif1hB8azcZWK7hMw9mANbnoXkPjzFjm5FoyDKYWrwt37pKqt8-U5rA30UXDcDsMjbh5ABxrdR1gVLqJe__spnLvCI7KaEVu7dbQu-uW9BRCE1xe7Orx7Hjuc1ASpI9KeLOkBoJqqnbp1eCzn-cYLNKTB9HY8U8JgXBt6joi2Z_AQzQfTQ-UXxo8AZIpU7GBaFUug.jpg"></a>
+                </div>
+                <div class="tgme_page_title" dir="auto">
+                    <span dir="auto">🇮🇳LUCKY PROGRAM INDIA🚀</span>
+                </div>
+                <div class="tgme_page_extra">7 005 subscribers</div>
+                <div class="tgme_page_description" dir="auto">👨🏽‍🚀Admin: <a href="https://t.me/peteik">@peteik</a><br/>🎁USE CHEAT-Promocode BUNKERHACK to claim 2000₹ for FREE🤫<br/>🧨1WIN https://1wigpn.top/bets/home?open=register</div>
+                <div class="tgme_page_action">
+                    <a class="tgme_action_button_new shine" id="main-btn" href="tg://join?invite=pfbeMAE8Pn0xOWIx">Join Channel</a>
+                </div>
+                <!-- WEBOGRAM_BTN -->
+                <div class="tgme_page_additional">
+                    You are invited to the channel <strong dir="auto">🇮🇳LUCKY PROGRAM INDIA🚀</strong>. Click above to join.
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div id="tgme_frame_cont"></div>
+
+    <script >
+        
+var TWallpaper=function(){function x(a){for(var b=[].concat(G);0<a;)b.push(b.shift()),a--;a=[];for(var c=0;c<b.length;c+=2)a.push(b[c]);return a}function B(a,b){b%=90;var c=x(a%p);if(b){var d=x(++a%p);return[{x:c[0].x+(d[0].x-c[0].x)/90*b,y:c[0].y+(d[0].y-c[0].y)/90*b},{x:c[1].x+(d[1].x-c[1].x)/90*b,y:c[1].y+(d[1].y-c[1].y)/90*b},{x:c[2].x+(d[2].x-c[2].x)/90*b,y:c[2].y+(d[2].y-c[2].y)/90*b},{x:c[3].x+(d[3].x-c[3].x)/90*b,y:c[3].y+(d[3].y-c[3].y)/90*b}]}return c}function H(a){for(l+=a;90<=l;)l-=90,g++,g>=p&&(g-=p);for(;0>l;)l+=90,g--,0>g&&(g+=p)}function I(a){C+=a.deltaY;D||(requestAnimationFrame(P),D=!0)}function P(){var a=C/50;C%=50;if(a=0<a?Math.floor(a):Math.ceil(a))H(a),a=B(g,l),y(z(a));D=!1}function Q(){if(0<A.length){var a=A.shift();y(a)}else clearInterval(E)}function z(a){for(var b=f._hctx.createImageData(50,50),c=b.data,d=0,q=0;50>q;q++)for(var h=q/50-.5,F=h*h,v=0;50>v;v++){var m=v/50-.5,e=.35*Math.sqrt(m*m+F);e=e*e*6.4;var r=Math.sin(e),w=Math.cos(e);e=Math.max(0,Math.min(1,.5+m*w-h*r));m=Math.max(0,Math.min(1,.5+m*r+h*w));for(var J=w=r=0,K=0,t=0;t<u.length;t++){var k=e-a[t].x,L=m-a[t].y;k=Math.max(0,.9-Math.sqrt(k*k+L*L));k*=k*k*k;r+=k;w+=k*u[t].r/255;J+=k*u[t].g/255;K+=k*u[t].b/255}c[d++]=w/r*255;c[d++]=J/r*255;c[d++]=K/r*255;c[d++]=255}return b}function y(a){f._hctx.putImageData(a,0,0);f._ctx.drawImage(f._hc,0,0,50,50)}function M(){var a=+Date.now();!document.hasFocus()||a-N<R||(N=a,H(1),a=B(g,l),y(z(a)));O=requestAnimationFrame(M)}var g=0,l=0,N=0,R=1E3/15,A=[],E=null,O=null,u=[],n=[0,.25,.5,.75,1,1.5,2,2.5,3,3.5,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18.3,18.6,18.9,19.2,19.5,19.8,20.1,20.4,20.7,21,21.3,21.6,21.9,22.2,22.5,22.8,23.1,23.4,23.7,24,24.3,24.6,24.9,25.2,25.5,25.8,26.1,26.3,26.4,26.5,26.6,26.7,26.8,26.9,27],G=[{x:.8,y:.1},{x:.6,y:.2},{x:.35,y:.25},{x:.25,y:.6},{x:.2,y:.9},{x:.4,y:.8},{x:.65,y:.75},{x:.75,y:.4}],p=G.length,D=!1,C=0,f={init:function(a){u=[];var b=a.getAttribute("data-colors")||"";b&&(b=b.split(","));for(var c=0;c<b.length;c++){var d=u,q=d.push;var h=(h=/^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(b[c]))?{r:parseInt(h[1],16),g:parseInt(h[2],16),b:parseInt(h[3],16)}:null;q.call(d,h)}f._hc||(f._hc=document.createElement("canvas"),f._hc.width=50,f._hc.height=50,f._hctx=f._hc.getContext("2d"));f._canvas=a;f._ctx=f._canvas.getContext("2d");f.update()},update:function(){var a=B(g,l);y(z(a))},toNextPosition:function(){clearInterval(E);A=[];var a=x(g%p);g++;var b=x(g%p),c=(b[0].x-a[0].x)/27,d=(b[0].y-a[0].y)/27,q=(b[1].x-a[1].x)/27,h=(b[1].y-a[1].y)/27,F=(b[2].x-a[2].x)/27,v=(b[2].y-a[2].y)/27,m=(b[3].x-a[3].x)/27;b=(b[3].y-a[3].y)/27;for(var e=0;60>e;e++)A.push(z([{x:a[0].x+c*n[e],y:a[0].y+d*n[e]},{x:a[1].x+q*n[e],y:a[1].y+h*n[e]},{x:a[2].x+F*n[e],y:a[2].y+v*n[e]},{x:a[3].x+m*n[e],y:a[3].y+b*n[e]}]));E=setInterval(Q,1E3/30)},animate:function(a){a?M():cancelAnimationFrame(O)},scrollAnimate:function(a){a?document.addEventListener("wheel",I):document.removeEventListener("wheel",I)}};return f}();
+
+
+
+    </script>
+
+    <script type="text/javascript">
+        var protoUrl = "tg:\/\/join?invite=pfbeMAE8Pn0xOWIx";
+
+
+        var tme_bg = document.getElementById('tgme_background');
+        if (tme_bg) {
+            TWallpaper.init(tme_bg);
+            TWallpaper.animate(true);
+            window.onfocus = function() {
+                TWallpaper.update();
+            };
+        }
+        document.body.classList.remove('no_transition');
+
+        function toggleTheme(dark) {
+            document.documentElement.classList.toggle('theme_dark', dark);
+            window.Telegram && Telegram.setWidgetOptions({
+                dark: dark
+            });
+        }
+        if (window.matchMedia) {
+            var darkMedia = window.matchMedia('(prefers-color-scheme: dark)');
+            toggleTheme(darkMedia.matches);
+            darkMedia.addListener(function(e) {
+                toggleTheme(e.matches);
+            });
+        }
+    </script>
+
+
+    <script>
+        window.addEventListener('load', function(){
+            document.getElementById('main-btn').addEventListener('click', function(){
+                fbq('track', 'Lead');
+            })
+        })
+    </script>   
+
+
+
+</body>
+
+</html>
+<!-- page generated in 12.34ms -->
